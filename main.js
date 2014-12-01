b9cee287c5ceca3c61b5f7da118a2f06f68abbd1
Cody Erekson <cody@erekson.org>
2014-11-30 23:34:37 -0700

    Create basic module structure

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Control module for PicoBorg Reverse
+ * https://www.piborg.org/picoborgrev
+ *
+ * by Cody Erekson
+ */
+
+var picoborgrev = require('./lib/picoborgrev.js');
+
+module.exports = picoborgrev;
